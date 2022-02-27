9e019ad6f96e22270e335fbd8fade10cd747bb3b
mentextu1986 <1445778002@qq.com>
2022-02-27 10:29:29 +0800

    任意子棋

---
+++ 22_2_17_three_in_a_row/22_2_17_three_in_a_row/function.c
@@ -0,0 +1,187 @@
+#include"game.h"
+extern int ro;
+extern int co;
+//开始界面及选择
+int cover()
+{
+	int a = 0;
+	printf("***** 1 single play *******\n");
+	printf("***** 2 double play *******\n");
+	printf("***** 0 over **************\n");
+	printf("请输入：\n");
+	scanf("%d", &a);
+	return a;
+}
+//棋盘初始化
+void clean(char board[row][col])
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+//棋盘打印
+void print_board(char board[row][col])
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j <col ; j++)
+		{
+			if (j <col- 1)
+				printf(" %c |",board[i][j]);
+			else
+				printf(" %c \n",board[i][j]);
+		}
+		if (i == row - 1)
+			break;
+		for (j = 0; j < col; j++)
+		{
+			if (j < col - 1)
+				printf("---|");
+			else
+				printf("---\n");
+		}
+	}
+}
+//p1玩家手动下棋,输入-1，-1认输
+int p_hand(char board[row][col],char a)
+{
+	while (1)
+	{
+		scanf("%d%d", &ro, &co);
+		ro--;
+		co--;
+		if ((ro < row) && (0 <= ro) && (co < col) && (0 <= co))
+		{
+			if (board[ro][co] == ' ')
+			{
+				board[ro][co] = a;
+				break;
+			}
+			else
+				printf("此处已有落子，请重新选择\n");
+		}
+		else if ((ro == 99) && (co == 99))
+			return 0;
+		else
+			printf("范围超出棋盘，请重新选择，不可超过%d行%d列\n", row, col);
+		
+	}
+	return 1;
+
+}
+char judge(char board[row][col])
+{
+	int a = 1;
+	int roo;
+	int coo;
+	//判断列是否获胜
+	{
+		for (roo = ro - 1; 0 <= roo; roo--)
+		{
+			if (board[roo][co] == board[ro][co])
+				a++;
+			else
+				break;
+		}
+		for (roo = ro +1; roo < row; roo++)
+		{
+			if (board[roo][co] == board[ro][co])
+				a++;
+			else
+				break;
+		}
+		if (a == num)
+			return board[ro][co];
+
+	}
+	//判断行是否获胜
+	{
+		a = 1;
+		for (coo = co - 1; 0<=coo; coo--)
+		{
+			if (board[ro][coo] == board[ro][co])
+				a++;
+			else break;
+		}
+		for (coo = co + 1; coo < col; coo++)
+		{
+			if (board[ro][coo] == board[ro][co])
+				a++;
+			else
+				break;
+		}
+
+		if (a == num)
+			return board[ro][co];
+	}
+	//判断↙是否胜利
+	{
+		a = 1;
+		for (roo = ro - 1, coo = co + 1; (0<= roo) && (coo <col); roo--, coo++)
+		{
+			if (board[roo][coo] == board[ro][co])
+				a++;
+			else break;
+		}
+		for (roo = ro + 1, coo = co - 1; (roo < row) && (0 <= coo); roo++, coo--)
+		{
+			if (board[roo][coo] == board[ro][co])
+				a++;
+			else
+				break;
+		}
+		if (a == num)
+			return board[ro][co];
+	}
+	//判断↘是否胜利
+	{
+		a = 1;
+		for (roo = ro - 1, coo = co - 1; (0 <= roo) && (0 <= coo); roo--, coo--)
+		{
+			if (board[roo][coo] == board[ro][co])
+				a++;
+			else
+				break;
+		}
+		for (roo = ro + 1, coo = co + 1; (roo <row) && (coo < col);roo++,coo++)
+		{
+			if (board[roo][coo] == board[ro][co])
+				a++;
+			else
+				break;
+		}
+		if (a == num)
+			return board[ro][co];
+	}
+	return '0';
+}
+
+void comp(char board[row][col], char a)
+{
+	while (1)
+	{
+	ro = rand() % (row + 1);
+	co = rand() % (row + 1);
+	if ((ro <row) && (0 <= ro) && (co < col) && (0 <= co))
+	{
+		if (board[ro][co] == ' ')
+		{
+			board[ro][co] = a;
+			break;
+		}
+		//else
+		//	printf("此处已有落子，请重新选择\n");
+	}
+	//else
+	//		printf("范围超出棋盘，请重新选择，不可超过%d行%d列\n", row, col);
+	}
+
+}
---
+++ 22_2_17_three_in_a_row/22_2_17_three_in_a_row/game.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define row 3
+#define col 3
+#define num 3
+#define p1sign 'O'
+#define p2sign 'X'
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int cover();
+void clean(char board[row][col]);
+void print_board(char board[row][col]);
+int p_hand(char board[row][col],char a);
+char judge(char board[row][col]);
+void comp(char board[row][col], char a);
